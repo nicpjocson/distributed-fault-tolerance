c62fcf7488b368978668482dae2d0b32d048994f
aaronjardenil <aaron_randall_jardenil@dlsu.edu.ph>
2025-04-13 22:46:47 +0800

    added frontnode gradle project

---
+++ FrontEndNode/src/main/java/com/stdiscm/p4/FrontEndNode/FrontEndNodeApplication.java
@@ -0,0 +1,13 @@
+package com.stdiscm.p4.FrontEndNode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontEndNodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrontEndNodeApplication.class, args);
+	}
+
+}
---
+++ FrontEndNode/src/test/java/com/stdiscm/p4/FrontEndNode/FrontEndNodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stdiscm.p4.FrontEndNode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontEndNodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
